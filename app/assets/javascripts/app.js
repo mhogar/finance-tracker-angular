4c088c1b37654e4ccbce139e7f8d76581d7edd96
Rails Udemy Course <railsudemycourse@gmail.com>
2018-10-30 02:00:09 +0000

    Addind stock API and services

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,37 @@
-var app = angular.module('FinanceTrackerApp', []);
+var app = angular.module('FinanceTrackerApp', [])
+                 .factory('stockService', ['$http', function($http) {
+                   var stockApi = {};
+                   
+                   stockApi.searchStocks = function(symbol) {
+                     return $http.get('/search_stocks.json?stock=' + symbol);
+                   }
+                   
+                   return stockApi;
+                 }])
+                 .controller('stocksController', ['$scope', 'stockService', function($scope, stockService) {
+                    $scope.stock = {};
+                   
+                    $scope.lookup = function() {
+                      if ($scope.ticker != undefined && $scope.ticker != '') {
+                        stockService.searchStocks($scope.ticker).then(
+                          function (response) {
+                            let data = response.data;
+                            
+                            $scope.stock = {};
+                            $scope.stock.error        = null;
+                            $scope.stock.symbol       = data.ticker;
+                            $scope.stock.name         = data.name;
+                            $scope.stock.last_price   = data.last_price;
+                            $scope.stock.can_be_added = data.can_be_added;
+                          },
+                          function (response) {
+                            $scope.stock = {};
+                            $scope.stock.error = response.data.response;
+                          }
+                        );
+                      }
+                      else {
+                       $scope.stock = {};
+                      }
+                    }
+                 }])
